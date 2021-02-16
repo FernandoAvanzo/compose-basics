93f9356941496534fe65dc655c8b22e0f4427d14
favanzo <f.avanzo@cinq.com.br>
2021-02-16 12:03:25 -0300

    #Code labs Compose Basics step 03-04

--- app/src/main/java/jetpack/compose/composebasics/MainActivity.kt
+++ app/src/main/java/jetpack/compose/composebasics/MainActivity.kt
@@ -1,38 +1,54 @@
 package jetpack.compose.composebasics
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.activity.compose.setContent
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import jetpack.compose.composebasics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyScreenContent()= Column {
+    Greeting("Jaguara")
+    Divider(color = Color.Black)
+    Greeting("there")
 }
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) = BasicsCodelabTheme {
+    Surface(color = Color.Yellow) {
+        content()
+    }
+}
+
+
+@Composable
+fun Greeting(name: String = "") = Text(
+    text = "Hello $name",
+    modifier = Modifier.padding(24.dp)
+)
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() = MyApp {
+    MyScreenContent()
 }
