49aeac4e873cff7e9c5295fe22c8dd1b20a4e89c
favanzo <f.avanzo@cinq.com.br>
2021-02-16 15:52:09 -0300

    #Code labs Compose Basics step 05

--- app/src/main/java/jetpack/compose/composebasics/MainActivity.kt
+++ app/src/main/java/jetpack/compose/composebasics/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -30,24 +31,39 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Jaguara", "there")) = Column {
-    val count = remember { mutableStateOf(0) }
-    names.map {
-        Greeting(it)
-        Divider(color = Color.Black)
-    }
-    Divider(color = Color.Transparent, thickness = 32.dp)
-    Counter(count.value) {
-        count.value = it
+fun MyScreenContent(
+    names: List<String> = List(1000) { "Hello Jaguara =>$it" },
+    stateCounter: MutableState<Int> = remember { mutableStateOf(0) }
+) = Column(modifier = Modifier.fillMaxHeight()) {
+    NameList(names = names, Modifier.weight(1f))
+    Counter(stateCounter.value) {
+        stateCounter.value = it
     }
 }
 
 @Composable
-fun Counter(count: Int, counter: (Int) -> Unit) {
-    Button(onClickAction(count = count, counter = counter)) {
+fun NameList(names: List<String>, modifier: Modifier = Modifier) =
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name)
+            Divider(color = Color.Black)
+        }
+    }
+
+@Composable
+fun Counter(count: Int, counter: (Int) -> Unit) =
+    Button(
+        onClickAction(count = count, counter = counter),
+        colors = ButtonDefaults.buttonColors(
+            colorButtonRule(count = count)
+        )
+    ) {
         Text("I've been clicked $count times")
     }
-}
+
+
+fun colorButtonRule(count: Int) = count
+    .takeIf { it > 5 }?.let { Color.Green } ?: Color.White
 
 fun onClickAction(count: Int, counter: (Int) -> Unit): () -> Unit = {
     counter(count + 1)
