ad9667026ff53070ad470c71d788cbe7dbd885fa
favanzo <f.avanzo@cinq.com.br>
2021-02-16 14:24:37 -0300

    #Code labs Compose Basics step 05

--- app/src/main/java/jetpack/compose/composebasics/MainActivity.kt
+++ app/src/main/java/jetpack/compose/composebasics/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
